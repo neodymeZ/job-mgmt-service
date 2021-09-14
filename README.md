1b048ed6de4a45d8456fb27fed038a6be6343a74
IZA <neodyme@ya.ru>
2021-09-14 11:29:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Job Management Service
+
+Description
---
+++ src/main/java/com/iza/SampleApp.java
@@ -0,0 +1,13 @@
+package com.iza;
+
+/**
+ * Hello world!
+ *
+ */
+public class SampleApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
